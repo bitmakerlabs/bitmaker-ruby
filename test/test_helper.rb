eef99da2e0f99b77a7eb7a2676362a0f682da858
Mina Mikhail <mina@fightingtheboss.com>
2017-05-26 17:13:39 -0400

    Require byebug and mocha for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,13 @@
+# Suppress Ruby 2.4 warnings
+$VERBOSE = nil
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bitmaker'
 
 require 'minitest/reporters'
 require 'minitest/autorun'
-require 'minitest/byebug' if ENV['DEBUG']
+require 'mocha/mini_test'
+require 'byebug'
 
 require 'webmock/minitest'
 require 'timecop'
